27fa84f859b45d39303f0c1cf278536489b9f3ba
SomalRudra <somalchakrabortyy@gmail.com>
2020-06-05 00:21:55 +0530

    Taglib Test

---
+++ lib/src/main/java/com/musicbrainz/ktaglib/AudioFile.kt
@@ -0,0 +1,18 @@
+package com.musicbrainz.ktaglib
+
+data class AudioFile(
+    val path: String = "",
+    var size: Long = 0,
+    var lastModified: Long = 0,
+    val title: String? = "",
+    val albumArtist: String? = "",
+    val artist: String? = "",
+    val album: String? = "",
+    val track: Int? = 0,
+    val trackTotal: Int? = 0,
+    val disc: Int? = 0,
+    val discTotal: Int? = 0,
+    val duration: Int? = 0,
+    val year: Int? = 0,
+    var genre: String? = ""
+)
